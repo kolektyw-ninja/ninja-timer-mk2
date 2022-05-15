6c1cc9b84091203a39aa3f44ec73a54698151785
LiquidLemon <liquidlemon@protonmail.com>
2022-05-15 16:14:05 +0200

    Add basic communication

--- src/display.rs
+++ src/display.rs
@@ -2,9 +2,9 @@ extern crate sdl2;
 
 use std::time::Duration;
 use std::path::Path;
+use std::sync::mpsc::{self, TryRecvError};
 
-use sdl2::ttf;
-use sdl2::ttf::Font;
+use sdl2::ttf::{self, Font};
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
@@ -12,49 +12,75 @@ use sdl2::rect::{Rect, Point};
 use sdl2::render::{Canvas, TextureQuery};
 use sdl2::video::Window;
 
-pub fn show_windows() -> Result<(), String> {
-    let sdl_context = sdl2::init()?;
-    let ttf_context = ttf::init().map_err(|e| e.to_string())?;
-    let video_subsystem = sdl_context.video()?;
-    let font = ttf_context.load_font(Path::new("./static/Inconsolata-Medium.ttf"), 64)?;
+pub struct Display {
+    receiver: mpsc::Receiver<String>,
+    text: String,
+}
 
-    let window = video_subsystem
-        .window("ninja-timer", 800, 600)
-        .position_centered()
-        .opengl()
-        .build()
-        .map_err(|e| e.to_string())?;
+impl Display {
+    pub fn new(receiver: mpsc::Receiver<String>) -> Self {
+        Self {
+            receiver,
+            text: String::from("hello world"),
+        }
+    }
 
-    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+    pub fn show_windows(&mut self) -> Result<(), String> {
+        let sdl_context = sdl2::init()?;
+        let ttf_context = ttf::init().map_err(|e| e.to_string())?;
+        let video_subsystem = sdl_context.video()?;
+        let font = ttf_context.load_font(Path::new("./static/Inconsolata-Medium.ttf"), 64)?;
 
-    canvas.set_draw_color(Color::RGB(0, 0, 0));
-    canvas.clear();
-    canvas.present();
-    let mut event_pump = sdl_context.event_pump()?;
+        let window = video_subsystem
+            .window("ninja-timer", 800, 600)
+            .position_centered()
+            .opengl()
+            .build()
+            .map_err(|e| e.to_string())?;
 
-    'running: loop {
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
-                _ => {}
+        let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+        canvas.clear();
+        canvas.present();
+        let mut event_pump = sdl_context.event_pump()?;
+
+        'running: loop {
+            for event in event_pump.poll_iter() {
+                match event {
+                    Event::Quit { .. }
+                    | Event::KeyDown {
+                        keycode: Some(Keycode::Escape),
+                        ..
+                    } => break 'running,
+                    _ => {}
+                }
+            }
+
+            canvas.clear();
+            render_text(&self.text, &Point::new(400, 300), &font, &mut canvas)?;
+            canvas.present();
+            ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
+            self.handle_messages()?;
+        }
+
+        Ok(())
+    }
+
+    fn handle_messages(&mut self) -> Result<(), String> {
+        loop {
+            match self.receiver.try_recv() {
+                Ok(text) => self.text = text,
+                Err(TryRecvError::Empty) => break,
+                Err(TryRecvError::Disconnected) => return Err(String::from("Receiver disconnected")),
             }
         }
 
-        canvas.clear();
-        render_text("Hello world", &Point::new(400, 300), &font, &mut canvas)?;
-        canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
-        // The rest of the game loop goes here...
+        Ok(())
     }
-
-    Ok(())
 }
 
-fn render_text(text: &'static str, point: &Point, font: &Font, canvas: &mut Canvas<Window>) -> Result<(), String> {
+fn render_text(text: &str, point: &Point, font: &Font, canvas: &mut Canvas<Window>) -> Result<(), String> {
     let texture_creator = canvas.texture_creator();
 
     let surface = font
--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,31 @@ extern crate sdl2;
 mod display;
 
 use std::thread;
+use std::sync::mpsc;
+use std::time::Duration;
 
-use display::show_windows;
+use display::Display;
 
 pub fn main() -> Result<(), String> {
+    let (tx, rx) = mpsc::channel();
 
-    let handle = thread::spawn(|| {
-        show_windows().expect("Could not init windows");
+    let mut display = Display::new(rx);
+    let handle = thread::spawn(move || {
+        display.show_windows().expect("Could not init windows");
     });
+
+    tx.send(String::from("5")).unwrap();
+    thread::sleep(Duration::new(0, 1_000_000_000u32));
+    tx.send(String::from("4")).unwrap();
+    thread::sleep(Duration::new(0, 1_000_000_000u32));
+    tx.send(String::from("3")).unwrap();
+    thread::sleep(Duration::new(0, 1_000_000_000u32));
+    tx.send(String::from("2")).unwrap();
+    thread::sleep(Duration::new(0, 1_000_000_000u32));
+    tx.send(String::from("1")).unwrap();
+    thread::sleep(Duration::new(0, 1_000_000_000u32));
+    tx.send(String::from("0")).unwrap();
+    thread::sleep(Duration::new(0, 1_000_000_000u32));
 
     handle.join().unwrap();
     Ok(())
