7cd9f86a642a75b5e35ad3e5d7f7a405afcf036c
LiquidLemon <liquidlemon@protonmail.com>
2022-09-25 18:37:50 +0200

    fix: early exit in GPIO thread on non-ARM arch

--- src/gpio.rs
+++ src/gpio.rs
@@ -9,6 +9,11 @@ const PIN_NUMBER: u8 = 17;
 
 pub fn spawn_gpio(sender: Sender<InputEvent>) -> JoinHandle<()> {
     spawn(move || {
+        if !cfg!(target_arch = "arm") {
+            println!("arch != 'arm', skipping GPIO");
+            return
+        }
+
         let gpio = Gpio::new().unwrap();
         let mut pin = gpio.get(PIN_NUMBER).unwrap().into_input_pulldown();
         pin.set_interrupt(Trigger::Both).unwrap();
